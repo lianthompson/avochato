d3a69da8583835cbe48bf63652883c6afc8126b4
Lian <thompsonlian@yahoo.com>
2019-02-12 16:54:07 -0800

    clean up variables

--- js/index.js
+++ js/index.js
@@ -1,26 +1,21 @@
 const carouselImages = document.querySelectorAll('.carousel-image')
-const carouselImagesArray = Array.from(carouselImages);
-const currentSlide = document.querySelector('.current-slide');
-const currentSlideArray = Array.from(currentSlide);
 const track = document.querySelector('.carousel-container--inner');
-const previousBtn = document.querySelector('#previous');
-const nextBtn = document.querySelector('#next');
-const dots = document.querySelector('.dot-container')
-const dotsArray = Array.from(dots.children);
-const currentDot = dots.querySelector('.active');
+const slides = Array.from(track.children);
+const previousBtn = document.querySelector('#prevButton');
+const nextBtn = document.querySelector('#nextButton');
+const dotsNav = document.querySelector('.dot-container')
+const dots = Array.from(dotsNav.children);
 const current = 0;
 let i = 0;
 const time = 3000;
-const slides = Array.from(track.children);
-const slideWidth = slides[0].getBoundingClientRect().width;
 
 startCarousel();
 
 
 function reset() {
-    for (let i = 0; i < carouselImagesArray.length; i++) {
-        carouselImagesArray[i].style.display = 'none';
-        dotsArray[i].style.backround = "background: rgba(0, 0, 0, .25)";
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+        dots[i].style.backround = "background: rgba(0, 0, 0, .25)";
     }
 }
 
@@ -39,54 +34,65 @@ function changeImg() {
 
 changeImg();
 
+function updateDots(currentDot, targetDot) {
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+    console.log(currentDot)
+    console.log(targetDot)
+}
 
 function startCarousel() {
     reset();
-
-    carouselImagesArray[i].style.display = "block";
-    dotsArray[i].style.background = "rgba(0, 0, 0, .75)";
+    slides[i].style.display = "block";
+    dots[i].style.background = "rgba(0, 0, 0, .75)";
 }
 
 function slideLeft() {
     reset();
-    carouselImagesArray[i - 1].style.display = "block";
-    dotsArray[i - 1].style.background = "rgba(0, 0, 0, .75)";
+    slides[i - 1].style.display = "block";
+    dots[i - 1].style.background = "rgba(0, 0, 0, .75)";
     i--;
 }
 
 function slideRight() {
     reset();
-    carouselImagesArray[i + 1].style.display = "block";
-    dotsArray[i + 1].style.background = "rgba(0, 0, 0, .75)";
+    slides[i + 1].style.display = "block";
+    dots[i + 1].style.background = "rgba(0, 0, 0, .75)";
     i++;
 }
 
 previousBtn.addEventListener('click', function() {
     if(i === 0){
-        i = carouselImagesArray.length;
+        i = slides.length;
     }
     slideLeft();
 });
 
 nextBtn.addEventListener('click', function() {
-    if(i === carouselImagesArray.length - 1){
+    // const currentSlide = track.querySelector('.current-slide');
+    // const nextSlide = currentSlide.nextElementSibling;
+    //  const currentDot = dotsNav.querySelector('.current-slide');
+    //  const nextDot = currentDot.nextElementSibling;
+    if(i === slides.length - 1){
         i = -1;
     }
     slideRight();
+    // updateDots(currentDot, nextDot)
 });
 
 
 // grab carouselImagesArray index of clicked dot
 
-dots.addEventListener('click', e => {
+dotsNav.addEventListener('click', e => {
     const targetDot = e.target.closest('span');
     if (!targetDot) return;
     
     const currentSlide = track.querySelector('.current-slide');
-    const currentDot = dots.querySelector('.current-slide');
-    const targetIndex = dotsArray.findIndex(dot => dot === targetDot)
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const targetIndex = dots.findIndex(dot => dot === targetDot)
 
-    const targetSlide = carouselImagesArray[targetIndex];
+    const targetSlide = slides[targetIndex];
 
-    console.log(targetIndex, targetSlide)
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
 });
